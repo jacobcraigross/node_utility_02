920d1c6b4123e6a575c59a09a4753da4f0181296
Jacob Ross <survivaldawn@gmail.com>
2021-02-18 13:25:51 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,62 +1,29 @@
-// THE VUE INSTANCE
+#!/usr/bin/env node
+const fs = require('fs');
+const util = require('util');
+const chalk = require('chalk');
+const targetDir = process.argv[2] || process.cwd();
+const path = require('path');
+const {lstat} = fs.promises;
 
-new Vue({
-  el: "#events",
-  data:{
-    event:{ name: '', description: '', date:''},
-    events: []
-  },
-  mounted: function(){
-    this.$nextTick(function(){
-      this.fetchEvents();
-    });
-  },
-  methods:{
-    fetchEvents: function(){
-      var events = [
-        {
-          id: 1,
-          name: 'TIFF',
-          description: 'Toronto International Film Festival',
-          date: '2017-09-10'
-        },
-        {
-          id: 2,
-          name: 'The Witch Premiere',
-          description: 'The Witch comes to theatres.',
-          date: '2017-10-02'
-        },
-        {
-          id: 3,
-          name: 'SXSW',
-          description: 'Music, film, and interactive festival in Austin, TX.',
-          date: '2018-02-02'
-        },
-        {
-          id: 4,
-          name: 'TX Round Up',
-          description: 'Vintage hot rod and motorcycle festival in Austin, TX.',
-          date: '2018-02-02'
-        }
-      ];
-
-        // this.$http.get('events').then(function(events){
-        //   this.events = events;
-        // })
-
-
-      this.events = events;
-    },
-    addEvent: function(){
-      if(this.event.name){
-        this.events.push(this.event);
-        this.event= { name: '', description: '', date: ''};
-      }
-    },
-    deleteEvent: function(index){
-      if(confirm('Are you sure you want to delete this event?')){
-        this.events.splice(index,1 );
-      }
+fs.readdir(targetDir, async (err, fileNames) => {
+    if (err) {
+        console.log(err);
     }
-  }
+    const statPromises = fileNames.map(fileName => {
+        return lstat(path.join(targetDir, fileName));
+    });
+
+
+    const allStats = await Promise.all(statPromises);
+
+    
+    for (let stats of allStats) {
+        const index = allStats.indexOf(stats);
+        if (stats.isFile()) {
+            console.log(fileNames[index]);
+        } else {
+            console.log(chalk.bold(fileNames[index]));
+        }
+    }
 });
